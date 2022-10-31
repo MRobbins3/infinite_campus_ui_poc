3b1e3f4ae3a1f74f9c00d81b7084d89e69751815
MRobbins3 <44211962+MRobbins3@users.noreply.github.com>
2022-10-30 19:58:46 -0500

    Lint :|

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,40 +7,40 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button
-      type="button"
-      @click="count++"
-    >
-      count is {{ count }}
-    </button>
-    <v-card><v-row><v-col>Hello I am a col</v-col></v-row></v-card>
+    <div class="card">
+        <button
+            type="button"
+            @click="count++"
+        >
+            count is {{ count }}
+        </button>
+        <v-card><v-row><v-col>Hello I am a col</v-col></v-row></v-card>
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test HMR
+        </p>
+    </div>
+
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+        Check out
+        <a
+            href="https://vuejs.org/guide/quick-start.html#local"
+            target="_blank"
+        >create-vue</a>, the official Vue + Vite starter
     </p>
-  </div>
-
-  <p>
-    Check out
-    <a
-      href="https://vuejs.org/guide/quick-start.html#local"
-      target="_blank"
-    >create-vue</a>, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a
-      href="https://github.com/johnsoncodehk/volar"
-      target="_blank"
-    >Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">
-    Click on the Vite and Vue logos to learn more
-  </p>
+    <p>
+        Install
+        <a
+            href="https://github.com/johnsoncodehk/volar"
+            target="_blank"
+        >Volar</a>
+        in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">
+        Click on the Vite and Vue logos to learn more
+    </p>
 </template>
 
 <style scoped>
